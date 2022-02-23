d6ec2adbefb6353f8d2493be08f24d2c61ee06b6
jamalviswa <98383885+jamalviswa@users.noreply.github.com>
2022-02-23 17:43:27 +0530

    Update dbsql.js

--- db/dbsql.js
+++ db/dbsql.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("bookit-backend",  {
+const sequelize = new Sequelize("bookit-backend","root","5cLnX2XfTzg4ssf"  {
   dialect: "mysql",
   host: "localhost",
 });
